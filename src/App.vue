c252285f3a638fe70bfa0386cb653a5abc4f6bcd
exmachina33 <84528753+exmachina33@users.noreply.github.com>
2023-01-13 11:53:40 +0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -13,25 +13,7 @@ import IconList from "@/components/UI/BaseIcon/IconList";
 import AppLayout from "@/layouts/AppLayout";
 
 export default {
-  components: {AppLayout, IconList},
-  methods: {
-    getVideos() {
-      axios({
-        method: 'get',
-        url: 'https://www.googleapis.com/youtube/v3/search',
-        params: {
-          part: null,
-          key: 'AIzaSyBFT4Ez073LY53UTN_HAZtBQfE3weLiN80',
-          type: 'video',
-          q: 'мипонегерой'
-        }
-      })
-    }
-  },
-  mounted() {
-    // authUser({email: 'test@gmail.com', password: '123456'})
-    // this.getVideos()
-  }
+  components: {AppLayout, IconList}
 }
 </script>
 
